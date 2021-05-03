450f5ec2c0e904c46602672b622625cc4ca84c5f
AlexanderPierce11 <takaaki.110706@gmail.com>
2021-05-04 01:35:48 +0900

    add_hash_to_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
   validates :name, {presence:true}
   validates :email, {presence:true, uniqueness:true}
-  validates :password, {presence:true}
-
 
   def posts
     return Post.where(user_id: self.id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_105257) do
+ActiveRecord::Schema.define(version: 2021_05_03_160405) do
 
   create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 2021_05_03_105257) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
