54228a100e89f1f8b91c0b554968d38fc3093ac2
AlexanderPierce11 <takaaki.110706@gmail.com>
2021-05-03 23:26:36 +0900

    add_thumbup_btn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do  
 
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
