5fffd43463f65e8bff9c4b02b7ac390c0ede178f
AlexanderPierce11 <takaaki.110706@gmail.com>
2021-05-29 16:08:28 +0900

    n+1_home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,39 +1,39 @@
 class HomeController < ApplicationController
 
   def top
-    @posts = Post.includes(:user)
+    @posts = Post.all.includes(:user)
   end
 
   def usa
-    @posts = Post.where(country: "USA")
+    @posts = Post.where(country: "USA").includes(:user)
   end
 
   def uk
-    @posts = Post.where(country: "UK")
+    @posts = Post.where(country: "UK").includes(:user)
   end
 
   def canada
-    @posts = Post.where(country: "Canada")
+    @posts = Post.where(country: "Canada").includes(:user)
   end
 
   def australia
-    @posts = Post.where(country: "Australia")
+    @posts = Post.where(country: "Australia").includes(:user)
   end
 
   def newzealand
-    @posts = Post.where(country: "NewZealand")
+    @posts = Post.where(country: "NewZealand").includes(:user)
   end
 
   def southafrica
-    @posts = Post.where(country: "SouthAfrica")
+    @posts = Post.where(country: "SouthAfrica").includes(:user)
   end
 
   def malta
-    @posts = Post.where(country: "Malta")
+    @posts = Post.where(country: "Malta").includes(:user)
   end
 
   def philippines
-    @posts = Post.where(country: "thePhilippines")
+    @posts = Post.where(country: "thePhilippines").includes(:user)
   end
   
 end
