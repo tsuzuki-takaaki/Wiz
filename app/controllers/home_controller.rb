f55dc664a54569a32ffa0e4e5d7018b389d5734b
AlexanderPierce11 <takaaki.110706@gmail.com>
2021-05-29 13:15:30 +0900

    remove_n+1_problems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def top
-    @posts = Post.all
+    @posts = Post.includes(:user)
   end
 
   def usa
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,4 @@ class User < ApplicationRecord
 
   validates :name, {presence:true}
   validates :email, {presence:true, uniqueness:true}
-
-  def posts
-    return Post.where(user_id: self.id)
-  end
 end
