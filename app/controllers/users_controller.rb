8de27019194e28af07eaa48dc3270f0857aa040b
AlexanderPierce11 <takaaki.110706@gmail.com>
2021-06-14 17:16:31 +0900

    access_registration_of_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
 
   before_action :sign_in_required, only: [:show]
+  before_action :only_self_user, only: [:show]
 
   def show
     @user = User.find_by(id: params[:id])
     @posts = @user.posts
   end
 
+  def only_self_user
+    @user = User.find_by(id: params[:id])
+    if @user.id != current_user.id
+      flash[:notice] ="権限がありません"
+      redirect_to(root_path)
+    end
+  end
+  
 end
